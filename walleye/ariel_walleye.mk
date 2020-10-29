0a4562bd6ecc157b042e620ad136b34799815edd
Sava Mikalacki <mikalackis@gmail.com>
2020-10-29 09:25:48 +0100

    Make sure Ariel vendor is included first

--- walleye/ariel_walleye.mk
+++ walleye/ariel_walleye.mk
@@ -2,11 +2,13 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Inherit some common stuff.
+# We inherit Ariel first to be able to override some
+# AOSP default params (like PRODUCT_COPY_FILES)
+$(call inherit-product, vendor/ariel/config/ariel_common.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/google/muskie/walleye/lineage_walleye.mk)
-
-# Inherit some common stuff.
-$(call inherit-product, vendor/ariel/config/ariel_common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := ariel_walleye
